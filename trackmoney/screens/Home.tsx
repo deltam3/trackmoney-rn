12e9098706e1483e929d3e57c8394473d145b20f
deltam3 <practicect@proton.me>
2024-11-03 12:54:49 +0900

    Home Screen setup

--- trackmoney/screens/Home.tsx
+++ trackmoney/screens/Home.tsx
@@ -1,6 +1,14 @@
+import * as React from "react";
 import { Text, View } from "react-native";
+import { Category, Transaction } from "../types";
+import { useSQLiteContext } from "expo-sqlite";
 
 export default function Home() {
+  const [categories, setCategories] = React.useState<Category[]>();
+  const [transactions, setTransactions] = React.useState<Transaction[]>();
+
+  const db = useSQLiteContext();
+
   return (
     <View>
       <Text>Home Screen</Text>
